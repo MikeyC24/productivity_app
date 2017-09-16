2feb73500847eea78218c37a73349be201bd6d93
mikeyc24 <mgchapman24@gmail.com>
2017-09-16 13:50:37 -0400

    woking on finance class

--- FinanceClass.py
+++ FinanceClass.py
@@ -16,10 +16,11 @@ import sqlite3
 
 class FinanceActivity:
 
-	def __init__(self, db_location, cc_report_name, finance_out_report_name):
+	def __init__(self, db_location, db_name, cc_report_name, finance_out_report_name):
 		self.db_location = db_location
+		self.db_name= db_name
 		self.cc_report_name = cc_report_name
-		self. finance_out_report_name = finance_out_report_name
+		self.finance_out_report_name = finance_out_report_name
 
 	def user_menu(self):
 		print('''Welcome to the finance part of the app, here is where you will manage
@@ -29,10 +30,13 @@ new budget or goals, press 3 to see past history and analyze, and lastly
 press 4 to compare current progress to goals. enter 999 to quit''')
 		menu_input = 0
 		while menu_input != 999:
-			menu_input = int(input('please enter selection: '))
+			# gonna need to add something to resee menu
+			#try:
+			menu_input = int(input('please enter selection(999 to quit): '))
 			if menu_input == 1:
 				print('add items')
-				self.add_items_from_cc()
+				#self.add_items_from_cc()
+				self.add_items_non_cc()
 			elif menu_input ==  2:
 				print('add goals')
 			elif menu_input == 3:
@@ -43,22 +47,36 @@ press 4 to compare current progress to goals. enter 999 to quit''')
 				pass
 			else:
 				print('That was not a recongized command')
+			#except Exception as e:
+			#	print('That command was not reconigzed, are you sure it was a number')
 		else:
 			print('goodbye')
 
 	def add_items_from_cc(self):
 		try:
-			con = sqlite3.connect(self.db_location)
+			con = sqlite3.connect(self.db_location+ self.db_name)
 		except Exception as e:
-			print(' no current datatable exists', e)
-		try:
-			df_to_write_to = df.read_sql_query('SELECT * FROM %s' % (table), con)
-		except Exception as e:
-			print(' no current table exists', e)
+			print(' no current datatable exists,', e)
 		print(str(self.db_location+self.cc_report_name))
 		cc_df = pd.read_csv(self.db_location+self.cc_report_name)
-		cc_df['categories'] = 'none selected yet'
+		#pd.DatetimeIndex(cc_df['Trans Date'], format='%m%d%Y', inplace=True)
+		cc_df.set_index(pd.DatetimeIndex(cc_df['Trans Date']), inplace=True, drop=True)
+		cc_df.drop('Trans Date', axis=1, inplace=True)
+		cc_df['Amount'] = cc_df['Amount'].apply(lambda x: x * -1)
+		print(cc_df.index)
+		#cc_df.sort_values(by=index)
+		cc_df.sort_index(ascending=True, inplace=True)
+		print(cc_df.head(5))
+		cc_df['category'] = 'none selected yet'
 		cc_df['additional_info'] = 'None added'
+		order = input('''Curenetly the data is shown by most recen transactions, would you
+like to reverse that or keep it, type reverse to switch: ''')
+		if order == 'reverse':
+			print('Data has been reversed and will show oldest transactions')
+			cc_df.sort_index(ascending=False, inplace=True)
+			print(cc_df.head())
+		else:
+			print('Data will stay in current order')
 		for x in range(len(cc_df)):
 			print('''You will be shown each entry in the report. After each entry you will be asked
 to assign each charge to a category, which is mandatory. Then you will the option of adding
@@ -67,24 +85,55 @@ in a description, with a limit of 50 characters.''')
 			print('''For the charge what category will you like to enter. enter "list", 
 to see all cateogiries currently entered''')
 			user_input = input('please enter a category: ')
-			"""
-			while cc_df[categories]
-			if user_input == 'list':
-				try:
-					print(cc_df['categories'].values.unique())
-				except Exception as e:
-					print('There appear to be no categories entered, or you have this error',e)
-			"""
-			cc_df['categories'].iloc[x] =  user_input
+			cc_df['category'].iloc[x] =  user_input
 			user_input1 = input('please add in any additional data: ')
 			if user_input1 is not None:
 				cc_df['additional_info'].iloc[x] = user_input1
-		print(cc_df.head(5))
+		print('Here are the changes you made, would you like to add additions to table? ')
+		print(cc_df.head())
+		add_to_db = input('type yes to add: ')
+		if add_to_db == 'yes':
+			if order == 'reverse':
+				cc_df.sort_index(ascending=True, inplace=True)
+			print('added to database')
+			cc_df.to_sql(self.finance_out_report_name, con, if_exists='append')
+		else:
+			print('changes have not been added')
+
+	def add_items_non_cc(self):
+		print('Here we will add cash and cash related expenses')
+		user_select = 0
+		date_array = []
+		amount_array = []
+		descrip_array = []
+		user_select = input('''Type quit if there is nothing to add in cash form,
+other press enter: ''')
+		while user_select != 'quit':
+			date = input('please enter the date of expense in the format m/d/yyyy: ')
+			amount = input('please enter the amount: ')
+			descrip = 'None given'
+			descrip = input('please enter description of expense: ')
+			date_array.append(date)
+			amount_array.append(int(amount))
+			descrip_array.append(descrip)
+			user_select = input('''press any key to add another item, type quit to go back
+, type add to add to database: ''')
+			if user_select == 'add':
+				df = pd.DataFrame({'Date': date_array, 'Amount':amount_array, 
+					'Description': descrip_array})
+				df.set_index(pd.DatetimeIndex(df['Date']), inplace=True, drop=True)
+				df.sort_index(ascending=True, inplace=True)
+				print(df.head(10))
+				user_select = 'quit'
+		else:
+			print('now going back')
 
 
 
 db_location = '/home/mike/Documents/coding_all/productivity_app/'
-cc_report_name = 'sample_cc_report.csv'
-report_output = 'financial_data'
-instance = FinanceActivity(db_location, cc_report_name, report_output)
+db_name = 'finance_db'
+#cc_report_name = 'sample_cc_report.csv'
+cc_report_name = 'sample_cc_report2.csv'
+report_output = 'financial_data2'
+instance = FinanceActivity(db_location, db_name, cc_report_name, report_output)
 instance.user_menu()
